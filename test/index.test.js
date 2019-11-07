71645e2f86e7b4c909e3e3d476cdcd215a2b0bad
Richard Walker <digitalsadhu@gmail.com>
2019-11-07 14:11:10 +1300

    update client to match latest changes to server and assets.json file

--- test/index.test.js
+++ test/index.test.js
@@ -5,94 +5,144 @@ const Client = require('../');
 
 test('development mode true', t => {
     const c = new Client({
+        js: 'http://my-dev-assets/scripts.js',
+        css: 'http://my-dev-assets/styles.css',
         path: './test/mocks/assets-1.json',
-        development: true,
+        development: true
     });
 
-    t.same(JSON.parse(JSON.stringify(c.js)), [
-        {
-            type: 'module',
-            value: 'http://my-dev-assets/scripts.js',
-        },
-    ]);
-    t.same(JSON.parse(JSON.stringify(c.css)), [
-        {
-            type: 'text/css',
-            value: 'http://my-dev-assets/styles.css',
-            rel: 'stylesheet',
-        },
-    ]);
+    t.same(
+        JSON.parse(JSON.stringify(c.js)),
+        [
+            {
+                type: 'module',
+                value: 'http://my-dev-assets/scripts.js'
+            }
+        ],
+        'client.js should return JS assets object'
+    );
+    t.same(
+        JSON.parse(JSON.stringify(c.css)),
+        [
+            {
+                type: 'text/css',
+                value: 'http://my-dev-assets/styles.css',
+                rel: 'stylesheet'
+            }
+        ],
+        'client.css should return CSS assets object'
+    );
     t.end();
 });
 
-test('development mode true, custom properties', t => {
+test('development mode true, setting options', t => {
     const c = new Client({
+        js: 'http://my-dev-assets/scripts.js',
+        css: 'http://my-dev-assets/styles.css',
         path: './test/mocks/assets-2.json',
-        development: true,
+        development: true
     });
 
-    t.same(JSON.parse(JSON.stringify(c.js)), [
-        {
-            type: 'module',
-            defer: true,
-            value: 'http://my-dev-assets/scripts.js',
-        },
-    ]);
-    t.same(JSON.parse(JSON.stringify(c.css)), [
-        {
-            type: 'text/css',
-            value: 'http://my-dev-assets/styles.css',
-            rel: 'stylesheet',
-        },
-    ]);
+    t.same(
+        JSON.parse(JSON.stringify(c.js)),
+        [
+            {
+                value: 'http://my-dev-assets/scripts.js',
+                type: 'module',
+                defer: true
+            }
+        ],
+        'client.js should return JS assets object'
+    );
+    t.same(
+        JSON.parse(JSON.stringify(c.css)),
+        [
+            {
+                value: 'http://my-dev-assets/styles.css',
+                type: 'text/css',
+                rel: 'stylesheet'
+            }
+        ],
+        'client.css should return CSS assets object'
+    );
     t.end();
 });
 
 test('development mode false', t => {
     const c = new Client({
         path: './test/mocks/assets-1.json',
-        development: false,
+        development: false
     });
 
-    t.same(JSON.parse(JSON.stringify(c.js)), [
-        {
-            type: 'module',
-            value: 'http://localhost:4001/my-org/js/my-app-name/1.0.0/index.js',
-        },
-    ]);
-    t.same(JSON.parse(JSON.stringify(c.css)), [
-        {
-            type: 'text/css',
-            value:
-                'http://localhost:4001/my-org/css/my-app-name/1.0.0/index.css',
-            rel: 'stylesheet',
-        },
-    ]);
+    t.same(
+        JSON.parse(JSON.stringify(c.js)),
+        [
+            {
+                type: 'module',
+                value:
+                    'http://localhost:4001/my-org/pkg/my-app-name/1.0.0/main/index.js'
+            },
+            {
+                type: 'iife',
+                value:
+                    'http://localhost:4001/my-org/pkg/my-app-name/1.0.0/ie11/index.js'
+            }
+        ],
+        'client.js should return JS assets object'
+    );
+    t.same(
+        JSON.parse(JSON.stringify(c.css)),
+        [
+            {
+                type: 'text/css',
+                value:
+                    'http://localhost:4001/my-org/pkg/my-app-name/1.0.0/main/index.css',
+                rel: 'stylesheet'
+            }
+        ],
+        'client.css should return CSS assets object'
+    );
     t.end();
 });
 
-test('development mode false, using tagOptions', t => {
+test('development mode false, setting options', t => {
     const c = new Client({
         path: './test/mocks/assets-3.json',
-        development: false,
+        development: false
     });
 
-    t.same(JSON.parse(JSON.stringify(c.js)), [
-        {
-            type: 'module',
-            async: true,
-            defer: true,
-            value: 'http://localhost:4001/my-org/js/my-app-name/1.0.0/index.js',
-        },
-    ]);
-    t.same(JSON.parse(JSON.stringify(c.css)), [
-        {
-            type: 'text/css',
-            value:
-                'http://localhost:4001/my-org/css/my-app-name/1.0.0/index.css',
-            rel: 'stylesheet',
-            title: 'my title',
-        },
-    ]);
+    t.same(
+        JSON.parse(JSON.stringify(c.js)),
+        [
+            {
+                value:
+                    'http://localhost:4001/my-org/pkg/my-app-name/1.0.0/main/index.js',
+                async: true,
+                defer: true,
+                type: 'module'
+            },
+            {
+                value:
+                    'http://localhost:4001/my-org/pkg/my-app-name/1.0.0/ie11/index.js',
+                async: true,
+                defer: true,
+                type: 'iife'
+            }
+        ],
+        'client.js should return JS assets object'
+    );
+    t.same(
+        JSON.parse(JSON.stringify(c.css)),
+        [
+            {
+                title: 'my title',
+                value:
+                    'http://localhost:4001/my-org/pkg/my-app-name/1.0.0/main/index.css',
+                type: 'text/css',
+                rel: 'stylesheet'
+            }
+        ],
+        'client.css should return CSS assets object'
+    );
     t.end();
 });
