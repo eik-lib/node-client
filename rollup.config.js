a1705f9eb486e11405e116699e6959fae1861076
William Killerud <william.killerud@schibsted.com>
2024-08-06 13:55:32 +0200

    chore: lint fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,11 @@
 export default {
-    input: 'src/index.js',
-    external: [
-        '@eik/common',
-        'undici',
-        'abslog',
-        'path',
-    ],
-    output: [
-        {
-            exports: 'auto',
-            format: 'cjs',
-            file: 'dist/index.cjs',
-        },
-    ],
+	input: "src/index.js",
+	external: ["@eik/common", "undici", "abslog", "path"],
+	output: [
+		{
+			exports: "auto",
+			format: "cjs",
+			file: "dist/index.cjs",
+		},
+	],
 };
