25b83ffda7c1430c46e44967ffe9124daf4f78e4
Trygve Lie <173003+trygve-lie@users.noreply.github.com>
2026-05-29 15:38:35 +0200

    fix: Replace Tap with node.js test runner (#278)

--- test/image.test.js
+++ test/image.test.js
@@ -1,22 +1,21 @@
-/// <reference path="../types/index.d.ts" />
-
-import tap from "tap";
+import { test } from "node:test";
+import assert from "node:assert";
 
 import Eik from "../src/index.js";
 
 const FIXTURE_PATH = `${process.cwd()}/fixtures/eik-image.json`;
 
-tap.test("image namespace is mapped correctly to /img", async (t) => {
+test("image namespace is mapped correctly to /img", async () => {
 	const client = new Eik({
 		base: "/public/images",
 		path: FIXTURE_PATH,
 	});
 	await client.load();
 
-	t.equal("/img/eik-image-fixture/1.0.0", client.pathname);
+	assert.strictEqual("/img/eik-image-fixture/1.0.0", client.pathname);
 });
 
-tap.test("image asset is mapped correctly", async (t) => {
+test("image asset is mapped correctly", async () => {
 	const client = new Eik({
 		path: FIXTURE_PATH,
 	});
@@ -25,14 +24,13 @@ tap.test("image asset is mapped correctly", async (t) => {
 	const file = "/art/such.webp";
 	const resolved = client.file(file);
 
-	t.equal(
+	assert.strictEqual(
 		resolved.value,
 		`${client.server}/img/eik-image-fixture/1.0.0/art/such.webp`,
 	);
-	t.end();
 });
 
-tap.test('development mode is set to "true" - base is unset', async (t) => {
+test('development mode is set to "true" - base is unset', async () => {
 	const client = new Eik({
 		development: true,
 		path: FIXTURE_PATH,
@@ -42,24 +40,19 @@ tap.test('development mode is set to "true" - base is unset', async (t) => {
 	const file = "/art/such.webp";
 	const resolved = client.file(file);
 
-	t.equal(resolved.value, "/art/such.webp");
-	t.end();
+	assert.strictEqual(resolved.value, "/art/such.webp");
 });
 
-tap.test(
-	'Cdevelopment mode is set to "true" - base is set to absolute URL',
-	async (t) => {
-		const client = new Eik({
-			development: true,
-			base: "http://localhost:7777/img/",
-			path: FIXTURE_PATH,
-		});
-		await client.load();
+test('Cdevelopment mode is set to "true" - base is set to absolute URL', async () => {
+	const client = new Eik({
+		development: true,
+		base: "http://localhost:7777/img/",
+		path: FIXTURE_PATH,
+	});
+	await client.load();
 
-		const file = "/art/such.webp";
-		const resolved = client.file(file);
+	const file = "/art/such.webp";
+	const resolved = client.file(file);
 
-		t.equal(resolved.value, "http://localhost:7777/img/art/such.webp");
-		t.end();
-	},
-);
+	assert.strictEqual(resolved.value, "http://localhost:7777/img/art/such.webp");
+});
